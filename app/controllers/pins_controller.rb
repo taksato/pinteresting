2279fcdb23e26927ecd90a99074c29e60ca45237
Takashi Sato <takashi.71989@gmail.com>
2015-06-02 14:07:48 +0900

    updated pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @pins = Pin.all
