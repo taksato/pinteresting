3855f56e5638a8eacaf3a04df56bfc8325d3325b
Takashi Sato <takashi.71989@gmail.com>
2015-06-04 01:35:50 +0900

    added jquery masonry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601155343) do
+ActiveRecord::Schema.define(version: 20150603125954) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
