b87e724d8ce02afdfb6a46690a7d7b1afd18ab0f
Takashi Sato <takashi.71989@gmail.com>
2015-06-07 22:06:53 +0900

    edited about page

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    has_attached_file :image, :styles => { :medium => "700x700>", :thumb => "100x100>" }
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
     validates :image, presence: true
 
